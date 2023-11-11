6d69eb0c2ca95dacf6620327f11a719fe8056f79
Zakary Boven <zakaryboven@gmail.com>
2023-11-11 22:26:32 +1300

    Remove map migrations

--- src/Launcher/MapMigrations/MapMigrationProvider.cs
+++ src/Launcher/MapMigrations/MapMigrationProvider.cs
@@ -1,13 +1,10 @@
-﻿namespace Launcher.MapMigrations
-{
+﻿namespace Launcher.MapMigrations {
   public static class MapMigrationProvider
   {
     public static IMapMigration[] GetMapMigrations()
     {
       return new IMapMigration[]
       {
-        new ControlPointMapMigration(),
-        new GoldBountyMapMigration()
       };
     }
   }
